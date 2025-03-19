cbae34936a831d427ec802af07243fce867e057e
Ooh-Ao <chanachai.pmg.eng@gmail.com>
2025-03-20 05:35:49 +0700

    landing

--- Web-Manage/src/app/app.routes.ts
+++ Web-Manage/src/app/app.routes.ts
@@ -9,11 +9,16 @@ import { LandingLayoutComponent } from './shared/layouts/landing-layout/landing-
 import { AuthGuard } from './shared/services/auth.guard';
 import { LoginComponent } from './authentication/login/login.component';
 import { CoverComponent } from './components/authentication/reset-password/cover/cover.component';
+import { JobsLandingComponent } from './components/pages/jobs-landing/jobs-landing.component';
+import { LandingComponent } from './components/pages/landing/landing.component';
 
 export const App_Route: Route[] = [
-  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '', component: ContentLayoutComponent, children: content},
-
+  {
+    path: 'home',
+    component: LandingComponent
+  },
   {
     path: 'auth/login',
     component: LoginComponent
