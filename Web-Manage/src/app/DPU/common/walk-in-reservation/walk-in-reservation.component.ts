79dcbb5d9e485816f4873c5ea661aac611516bd0
Ooh-Ao <chanachai.pmg.eng@gmail.com>
2025-05-17 02:22:38 +0700

    no message

--- Web-Manage/src/app/DPU/common/walk-in-reservation/walk-in-reservation.component.ts
+++ Web-Manage/src/app/DPU/common/walk-in-reservation/walk-in-reservation.component.ts
@@ -5,12 +5,13 @@ import { FlatpickrModule, FlatpickrDefaults } from 'angularx-flatpickr';
 import flatpickr from 'flatpickr';
 import { MaterialModuleModule } from '../../../material-module/material-module.module';
 import { SharedModule } from '../../../shared/shared.module';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-walk-in-reservation',
   standalone: true,
-  imports: [SharedModule,NgSelectModule,MaterialModuleModule,FlatpickrModule,FormsModule,ReactiveFormsModule],
-  providers:[FlatpickrDefaults],
+  imports: [SharedModule, NgSelectModule, MaterialModuleModule, FlatpickrModule, FormsModule, ReactiveFormsModule],
+  providers: [FlatpickrDefaults],
   templateUrl: './walk-in-reservation.component.html',
   styleUrl: './walk-in-reservation.component.scss'
 })
@@ -18,7 +19,10 @@ export class WalkInReservationComponent {
   flatpickrOptions: any = {
     inline: true,
   };
-  ngOnInit(): void{
+  constructor(private http : HttpClient) {
+
+  }
+  ngOnInit(): void {
 
     this.flatpickrOptions = {
       enableTime: true,
@@ -28,12 +32,30 @@ export class WalkInReservationComponent {
 
     flatpickr('#inlinetime', this.flatpickrOptions);
 
-      this.flatpickrOptions = {
-        enableTime: true,
-        dateFormat: 'Y-m-d H:i', // Specify the format you want
-        defaultDate: '2023-11-07 14:30', // Set the default/preloaded time (adjust this to your desired time)
-      };
+    this.flatpickrOptions = {
+      enableTime: true,
+      dateFormat: 'Y-m-d H:i', // Specify the format you want
+      defaultDate: '2023-11-07 14:30', // Set the default/preloaded time (adjust this to your desired time)
+    };
 
-      flatpickr('#pretime', this.flatpickrOptions);
+    flatpickr('#pretime', this.flatpickrOptions);
+  }
+
+  saveReservation() {
+    let reservationData = {
+      "start_time": "2025-05-16T19:18:37.523Z",
+      "end_time": "2025-05-16T19:18:37.523Z",
+      "num_people": 0,
+      "user_id": 0,
+      "phone": "string",
+      "table_id": 0,
+      "room_id": 0,
+      "status": "pending"
+    }
+    this.http.post('http://localhost:8080/api/v1/reservation', reservationData).subscribe(
+      (response) => {
+        console.log('Reservation saved successfully!', response);
+      }
+    );
   }
 }
