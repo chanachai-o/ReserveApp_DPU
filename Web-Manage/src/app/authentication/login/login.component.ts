dd41d418fd692d37a0f991eb92fd60dcccf70a89
Ooh-Ao <chanachai.pmg.eng@gmail.com>
2025-06-01 01:49:06 +0700

    no message

--- Web-Manage/src/app/authentication/login/login.component.ts
+++ Web-Manage/src/app/authentication/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, Inject, Renderer2 } from '@angular/core';
 // import { AuthService } from 'src/app/shared/services/auth.service';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule ,Validators  } from '@angular/forms';
 import { CommonModule, DOCUMENT } from '@angular/common';
 import { CarouselModule, OwlOptions, SlidesOutputData } from 'ngx-owl-carousel-o';
 import { SharedModule } from '../../shared/shared.module';
@@ -10,10 +10,9 @@ import { TokenService } from '../../shared/services/token.service';
 import { AuthService } from '../../shared/services/auth.service';
 import { LoginModel } from '../../shared/user-auth.model';
 import { HttpClientModule } from '@angular/common/http';
-import { Validators } from 'ngx-editor';
 import { ProjectMemberService } from '../../DPU/services/project-members.service';
 import { ToastrService } from 'ngx-toastr';
-
+import swal from 'sweetalert';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -23,98 +22,27 @@ import { ToastrService } from 'ngx-toastr';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  phone = ""
-  password = ""
-  errorMessage = '';
-  showLoader = false;
-  _error: { name: string; message: string } = { name: '', message: '' }; // for firbase _error handle
-  public error: any = '';
-
-  public loginForm!: FormGroup;
+  form: FormGroup;
+  loading = false;
+  errorMessage: string = '';
 
   constructor(
-    @Inject(DOCUMENT) private document: Document, private elementRef: ElementRef,
-    private renderer: Renderer2,
-    public tokenService: TokenService,
-    private routes: Router,
-    private formBuilder: FormBuilder,
+    private fb: FormBuilder,
     public authService: AuthService,
-    private projectMember: ProjectMemberService
+    private routes: Router,
+    public tokenService: TokenService,
   ) {
-    localStorage.clear()
-  }
-  ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      phone: ['', Validators.required],
-      password: ['', Validators.required],
-    });
-    this.renderer.addClass(this.document.body, 'bg-white');
-    this.renderer.addClass(this.document.body, 'dark:bg-!bodybg');
-    const ltr = this.elementRef.nativeElement.querySelectorAll('#switcher-ltr');
-    const rtl = this.elementRef.nativeElement.querySelectorAll('#switcher-rtl');
-
-    fromEvent(ltr, 'click').subscribe(() => {
-      this.customOptions = { ...this.customOptions, rtl: false };
-    });
-
-    fromEvent(rtl, 'click').subscribe(() => {
-      this.customOptions = { ...this.customOptions, rtl: true, autoplay: true };
+    this.form = this.fb.group({
+      phone: ['', [Validators.required]],
+      password: ['', [Validators.required]]
     });
   }
-  ngOnDestroy(): void {
-    this.renderer.removeClass(this.document.body, 'bg-white');
-    this.renderer.removeClass(this.document.body, 'dark:bg-!bodybg');
-  }
-  customOptions: OwlOptions = {
-    loop: true,
-    rtl: false,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: false,
-    dots: true,
-    navSpeed: 700,
-    autoplay: true,
-    navText: ['<', '>'],
-    autoHeight: true,
-    autoWidth: true,
-    responsive: {
-      0: { items: 1 },
-      400: { items: 1 },
-      740: { items: 1 },
-      1000: { items: 1 },
-    },
-    nav: true,
-  };
 
-  activeSlides!: SlidesOutputData;
-
-  slidesStore: any[] = [
-    { img: './assets/images/authentication/2.png' },
-    { img: './assets/images/authentication/3.png' },
-    { img: "./assets/images/authentication/2.png" },
-  ];
-
-  getPassedData(data: SlidesOutputData) {
-    this.activeSlides = data;
-    console.log(this.activeSlides);
-  }
-  showPassword = false;
-  toggleClass = "off-line";
-  toggleVisibility() {
-    this.showPassword = !this.showPassword;
-    if (this.toggleClass === "off-line") {
-      this.toggleClass = "line";
-    } else {
-      this.toggleClass = "off-line";
-    }
-  }
-
-  login() {
-    this.showLoader = true;
-    let body: LoginModel = new LoginModel();
-    body.phone = this.phone;
-    body.password = this.password;
-    this.authService.login(this.loginForm.controls['phone'].value, this.loginForm.controls['password'].value).subscribe(result => {
+  onSubmit() {
+    if (this.form.invalid) return;
+    this.loading = true;
+    this.errorMessage = '';
+    this.authService.login(this.form.controls['phone'].value, this.form.controls['password'].value).subscribe(result => {
       this.tokenService.saveToken(result);
       // this.tokenService.saveRefreshToken(result.refreshToken);
       this.tokenService.saveUser(result);
@@ -123,32 +51,144 @@ export class LoginComponent {
           this.routes.navigate(['/admin/home'])
         } else if (result.role == 'staff') {
           this.routes.navigate(['/admin/walk-in'])
-        }  else if (result.role == 'customer') {
+        } else if (result.role == 'customer') {
           this.routes.navigate(['/customer/home-customer'])
         }
       } else {
-        this.error = 'ไม่สามารถใช้งานได้กรุณาติดต่อผู้ให้บริการ'
+        this.errorMessage = 'ไม่สามารถใช้งานได้กรุณาติดต่อผู้ให้บริการ'
       }
       // this.routes.navigate(['/admin/member-manage'])
-    }, (error) => {
-      this.showLoader = false;
-      this.error = 'เบอร์ติดต่อ หรือ Password ไม่ถูกต้อง';
-    })
-
-  }
-
-  adminCompanyList(memberId: string) {
-    this.projectMember.getCompanyAdmin(memberId).subscribe(result => {
-      if (result.length > 0) {
-        this.routes.navigate(['/company/home/' + result[0].projectId])
-      }
-      else {
-        this.error = 'คุณไม่มีสิทธิ์ใช้งานในส่วนนี้'
-      }
-    }, (error) => {
-      this.showLoader = false;
-      this.error = 'คุณไม่มีสิทธิ์ใช้งานในส่วนนี้';
+    }, (err) => {
+      this.loading = false;
+      this.errorMessage = err.error?.detail || 'เข้าสู่ระบบไม่สำเร็จ';
     })
   }
+  // phone = ""
+  // password = ""
+  // errorMessage = '';
+  // showLoader = false;
+  // _error: { name: string; message: string } = { name: '', message: '' }; // for firbase _error handle
+  // public error: any = '';
+
+  // public loginForm!: FormGroup;
+
+  // constructor(
+  //   @Inject(DOCUMENT) private document: Document, private elementRef: ElementRef,
+  //   private renderer: Renderer2,
+  //   public tokenService: TokenService,
+  //   private routes: Router,
+  //   private formBuilder: FormBuilder,
+  //   public authService: AuthService,
+  //   private projectMember: ProjectMemberService
+  // ) {
+  //   localStorage.clear()
+  // }
+  // ngOnInit(): void {
+  //   this.loginForm = this.formBuilder.group({
+  //     phone: ['', Validators.required],
+  //     password: ['', Validators.required],
+  //   });
+  //   this.renderer.addClass(this.document.body, 'bg-white');
+  //   this.renderer.addClass(this.document.body, 'dark:bg-!bodybg');
+  //   const ltr = this.elementRef.nativeElement.querySelectorAll('#switcher-ltr');
+  //   const rtl = this.elementRef.nativeElement.querySelectorAll('#switcher-rtl');
+
+  //   fromEvent(ltr, 'click').subscribe(() => {
+  //     this.customOptions = { ...this.customOptions, rtl: false };
+  //   });
+
+  //   fromEvent(rtl, 'click').subscribe(() => {
+  //     this.customOptions = { ...this.customOptions, rtl: true, autoplay: true };
+  //   });
+  // }
+  // ngOnDestroy(): void {
+  //   this.renderer.removeClass(this.document.body, 'bg-white');
+  //   this.renderer.removeClass(this.document.body, 'dark:bg-!bodybg');
+  // }
+  // customOptions: OwlOptions = {
+  //   loop: true,
+  //   rtl: false,
+  //   mouseDrag: true,
+  //   touchDrag: true,
+  //   pullDrag: false,
+  //   dots: true,
+  //   navSpeed: 700,
+  //   autoplay: true,
+  //   navText: ['<', '>'],
+  //   autoHeight: true,
+  //   autoWidth: true,
+  //   responsive: {
+  //     0: { items: 1 },
+  //     400: { items: 1 },
+  //     740: { items: 1 },
+  //     1000: { items: 1 },
+  //   },
+  //   nav: true,
+  // };
+
+  // activeSlides!: SlidesOutputData;
+
+  // slidesStore: any[] = [
+  //   { img: './assets/images/authentication/2.png' },
+  //   { img: './assets/images/authentication/3.png' },
+  //   { img: "./assets/images/authentication/2.png" },
+  // ];
+
+  // getPassedData(data: SlidesOutputData) {
+  //   this.activeSlides = data;
+  //   console.log(this.activeSlides);
+  // }
+  // showPassword = false;
+  // toggleClass = "off-line";
+  // toggleVisibility() {
+  //   this.showPassword = !this.showPassword;
+  //   if (this.toggleClass === "off-line") {
+  //     this.toggleClass = "line";
+  //   } else {
+  //     this.toggleClass = "off-line";
+  //   }
+  // }
+
+  // login() {
+  //   this.showLoader = true;
+  //   let body: LoginModel = new LoginModel();
+  //   body.phone = this.phone;
+  //   body.password = this.password;
+  //   this.authService.login(this.loginForm.controls['phone'].value, this.loginForm.controls['password'].value).subscribe(result => {
+  //     this.tokenService.saveToken(result);
+  //     // this.tokenService.saveRefreshToken(result.refreshToken);
+  //     this.tokenService.saveUser(result);
+  //     if (result.is_active) {
+  //       if (result.role == 'manager') {
+  //         this.routes.navigate(['/admin/home'])
+  //       } else if (result.role == 'staff') {
+  //         this.routes.navigate(['/admin/walk-in'])
+  //       }  else if (result.role == 'customer') {
+  //         this.routes.navigate(['/customer/home-customer'])
+  //       }
+  //     } else {
+  //       this.error = 'ไม่สามารถใช้งานได้กรุณาติดต่อผู้ให้บริการ'
+  //     }
+  //     // this.routes.navigate(['/admin/member-manage'])
+  //   }, (error) => {
+  //     this.showLoader = false;
+  //     this.error = 'เบอร์ติดต่อ หรือ Password ไม่ถูกต้อง';
+  //   })
+
+  // }
+
+  // adminCompanyList(memberId: string) {
+  //   this.projectMember.getCompanyAdmin(memberId).subscribe(result => {
+  //     if (result.length > 0) {
+  //       this.routes.navigate(['/company/home/' + result[0].projectId])
+  //     }
+  //     else {
+  //       this.error = 'คุณไม่มีสิทธิ์ใช้งานในส่วนนี้'
+  //     }
+  //   }, (error) => {
+  //     this.showLoader = false;
+  //     this.error = 'คุณไม่มีสิทธิ์ใช้งานในส่วนนี้';
+  //   })
+  // }
 }
 
