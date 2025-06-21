eee11c22fabf67f67eb94b39390ee98f06c1210e
Ooh-Ao <chanachai.pmg.eng@gmail.com>
2025-06-21 11:02:55 +0700

    เอา component ไม่ได้ใช้ออก

--- Web-Manage/src/app/app.routes.ts
+++ Web-Manage/src/app/app.routes.ts
@@ -2,14 +2,12 @@ import { Route } from '@angular/router';
 import { ContentLayoutComponent } from './shared/layouts/content-layout/content-layout.component';
 import { content } from './shared/routes/content.routes';
 import { AuthenticationLayoutComponent } from './shared/layouts/authentication-layout/authentication-layout.component';
-import { authen } from './shared/routes/auth.routes';
 import { landingRoutingModule } from './components/pages/landing.routes';
 import { landing } from './shared/routes/landing.routes';
 import { LandingLayoutComponent } from './shared/layouts/landing-layout/landing-layout.component';
 import { AuthGuard } from './shared/services/auth.guard';
 import { LoginComponent } from './authentication/login/login.component';
-import { CoverComponent } from './components/authentication/reset-password/cover/cover.component';
-import { JobsLandingComponent } from './components/pages/jobs-landing/jobs-landing.component';
+
 import { LandingComponent } from './components/pages/landing/landing.component';
 import { CustomerRegisterComponent } from './DPU/common/customer-register/customer-register.component';
 
@@ -27,9 +25,5 @@ export const App_Route: Route[] = [
   {
     path: 'auth/login',
     component: LoginComponent
-  },
-  {
-    path: 'auth/reset-password',
-    component: CoverComponent
   }
 ]
